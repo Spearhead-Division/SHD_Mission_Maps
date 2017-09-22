1c300f9093ff94bc186bb5a85d50d391bdf04ff6
Raychan <davidrueckert@hotmail.de>
2017-09-22 20:40:56 +0200

    Add files via upload

---
+++ userconfig/server/server.hpp
@@ -0,0 +1,2 @@
+force L_Suppress_enabled = true;
+force BWA3_NaviPad_showMembers = true;
---
+++ userconfig/task_force_radio/radio_settings.hpp
@@ -0,0 +1,19 @@
+﻿
+// =================================================
+// Server side only
+// =================================================
+force tf_no_auto_long_range_radio = 1;
+force TF_give_personal_radio_to_regular_soldier = 0;
+force TF_give_microdagr_to_soldier = 0;
+force tf_same_sw_frequencies_for_side = 0;
+force tf_same_lr_frequencies_for_side = 0;
+force tf_same_dd_frequencies_for_side = 0;
+// =================================================
+// END: Server side only
+// =================================================
+// Client side
+// =================================================
+tf_default_radioVolume = 9;
+// =================================================
+// END: Client side
+// =================================================
